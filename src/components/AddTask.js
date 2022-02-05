79afc1270492fe0357c6672008b2523215660cb6
chantelle mckenzie <mrspasceri@hotmail.com>
2022-02-04 20:27:09 -0800

    moved firebase into folder

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,7 +1,7 @@
 import Modal from "./Modal"
 import {useState} from 'react'
 import './addTask.css'
-import {db} from '../firebase'
+import {db} from '../firebase/firebase'
 import {collection, addDoc, Timestamp} from 'firebase/firestore'
 
 function AddTask({onClose, open}) {
