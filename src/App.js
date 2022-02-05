2f7c63e687b1b16096d8f5ed1d751cd5a5b0aa7e
chantelle mckenzie <mrspasceri@hotmail.com>
2022-02-04 20:00:15 -0800

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import Home from './components/TaskManager'
 
 function App() {
+
   return (
-    <div className="App">
-   
+    <div className='app'>
+      <Home />
     </div>
   );
 }
